4448f0622608f75974155d7a666b96f564b23dc6
acie-arsh <arshchaudhary903@gmail.com>
2024-10-13 15:33:15 +0530

    Auction Item routes and controller

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ import fileUpload from 'express-fileupload';
 import { connection } from './database/connection.js';
 import { errorMiddleware } from './middlewares/error.js';
 import userRouter from './router/userRoutes.js';
+import auctionItemRouter from './router/auctionItemRoutes.js';
 
 const app = express();
 config({
@@ -27,6 +28,7 @@ app.use(fileUpload({
 }));
 
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/auctionitem", auctionItemRouter);
 
 connection();
 
---
+++ backend/router/auctionItemRoutes.js
@@ -0,0 +1,39 @@
+import express from "express";
+import {
+  addNewAuctionItem,
+  getAllItems,
+  getAuctionDetails,
+  getMyAuctionItems,
+  removeAuctionItem,
+  republishAuctionItem,
+} from "../controllers/auctionItemController.js";
+import { isAuthenticated, isAuthorized } from "../middlewares/auth.js";
+
+const router = express.Router();
+
+router.post(
+  "/create",
+  isAuthenticated,
+  isAuthorized("Seller"),
+  addNewAuctionItem
+);
+
+router.get("/allitems", getAllItems);
+router.get("/auction/:id", isAuthenticated, getAuctionDetails);
+router.get("/myitems", isAuthenticated, isAuthorized("Seller"), getMyAuctionItems);
+
+router.delete(
+  "/delete/:id",
+  isAuthenticated,
+  isAuthorized("Seller"),
+  removeAuctionItem
+);
+
+router.put(
+  "/item/republish/:id",
+  isAuthenticated,
+  isAuthorized("Seller"),
+  republishAuctionItem
+);
+
+export default router;
